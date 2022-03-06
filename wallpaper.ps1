bb91fccaf70922d5edeaf4d2284653a8a56fbd87
Criptc <96071765+Criptc@users.noreply.github.com>
2022-03-06 14:29:56 -0600

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,2 +1,2 @@
-Set-ItemProperty -path 'HCU:\Control Panel\Desktop\' -name wallpaper -value C:\Users\$env:UserName\AppData\Local\Temp\img.jpg
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value C:\Users\$env:UserName\AppData\Local\Temp\img.jpg
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
