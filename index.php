aa9fb577d283633c872f68b89f69d8d4ddf7c374
Criptc <96071765+Criptc@users.noreply.github.com>
2022-04-02 19:37:14 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div id="main">
+                <h1>Login</h1>
+                <div id="login">
+                                                <form method="post" action="/index.php">
+                                <div>Username</div>
+                                <div><input type="text" name="login_usr"></div>
+                                <div>Password</div>
+                                <div><input type="password" name="login_pwd"></div>
+                                <div><input type="submit" name="login_btn" value="Login"></div>
+                        </form>
+                </div>
+        </div>
+</body>
+</html>
